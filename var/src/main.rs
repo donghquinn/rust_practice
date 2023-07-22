75b7fbf12329aab813428f14929feb9f8cfb39df
donghquinn <ehdgus1524@gmail.com>
2023-07-22 16:34:29 +0900

    wip

---
+++ var/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x = 32;
+    let y = 64;
+
+    println!("X+Y = {}", x + y);
+}
---
+++ var/target/debug/var.d
@@ -0,0 +1,1 @@
+/Users/kimdonghyun/Desktop/codes/rust/var/target/debug/var: /Users/kimdonghyun/Desktop/codes/rust/var/src/main.rs
