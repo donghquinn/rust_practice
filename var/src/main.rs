019c42fa8d49fb79792a4e0ac96f8557b5e32575
donghquinn <ehdgus1524@gmail.com>
2023-07-22 16:45:05 +0900

    wip

--- var/src/main.rs
+++ var/src/main.rs
@@ -13,6 +13,9 @@ fn tuple() {
     // 서로 다른 타입의 숫자형 튜플
     let tuples: (i32, f64, u32) = (100, 0.5, 2);
 
+    // 각각 튜플에 접근하는 방법
+    println!("Each Tuples: {}, {}, {}", tuples.0, tuples.1, tuples.2);
+
     // 같은 타입의 숫자형 튜플로 연산까지 -> 벡터에 사용하기 좋을 듯
     let t: (f64, f64, f64) = (100., 0.5, 2.);
     let (x, y, z) = t;
@@ -20,4 +23,13 @@ fn tuple() {
     let calculateResult = (x * y) * z;
 
     println!("Tuple Calculate Result: {}", calculateResult);
+}
+
+fn arrays() {
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    println!(
+        "Access to Array Values: 0 - {}, 1 - {}, 4 - {}, -1 - {}",
+        array[0], array[1], array[4], array[-1]
+    )
 }
