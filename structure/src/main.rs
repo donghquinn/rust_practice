79548254d6c89cfc97f96ec3dde07b245f67afe2
donghquinn <ehdgus1524@gmail.com>
2023-07-24 10:24:55 +0900

    structure

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -4,6 +4,11 @@ struct User {
     is_active: bool,
 }
 
+struct Rectangle {
+    width: f64,
+    hight: f64,
+}
+
 fn main() {
     let user_info = set_user(
         String::from("ehdgus1524@gmail.com"),
@@ -14,6 +19,14 @@ fn main() {
     println!("User Email: {}", user_info.email);
 
     println!("Square Value: {}", area((32., 54.)));
+
+    let rectangular = Rectangle {
+        width: 32.,
+        hight: 54.,
+    };
+
+    // rectangular 복사
+    println!("Rectangular Area Value: {}", area_2(&rectangular));
 }
 
 fn set_user(email: String, user: String) -> User {
@@ -24,6 +37,11 @@ fn set_user(email: String, user: String) -> User {
     }
 }
 
+// 튜플을 이용한 코드
 fn area(dimensions: (f64, f64)) -> f64 {
     dimensions.0 * dimensions.1
+}
+
+fn area_2(rectangle: &Rectangle) -> f64 {
+    rectangle.width * rectangle.hight
 }
