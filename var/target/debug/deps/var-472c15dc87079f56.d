2b8e0c57faee5a462e427e8cd14a1ad8d6761138
Kim Donghyun <kimdonghyun@Kims-MacBook-Air.local>
2023-12-14 12:49:10 +0900

    wip

--- var/target/debug/deps/var-472c15dc87079f56.d
+++
@@ -1,5 +0,0 @@
-/Users/kimdonghyun/Desktop/codes/rust/var/target/debug/deps/var-472c15dc87079f56: src/main.rs
-
-/Users/kimdonghyun/Desktop/codes/rust/var/target/debug/deps/var-472c15dc87079f56.d: src/main.rs
-
-src/main.rs:
--- var/target/debug/var.d
+++
@@ -1,1 +0,0 @@
-/Users/kimdonghyun/Desktop/codes/rust/var/target/debug/var: /Users/kimdonghyun/Desktop/codes/rust/var/src/main.rs
